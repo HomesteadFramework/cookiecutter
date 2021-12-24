e5f3a2665eeedbe699ea49f0515f9217919dfbd5
Brandon Braner <brandon.braner@gmail.com>
2021-12-23 21:12:25 -0600

    wip

---
+++ {{cookiecutter.app_name}}/debug.py
@@ -0,0 +1,9 @@
+import uvicorn
+
+
+if __name__ == '__main__':
+    """
+    Used to debug via pycharm configuration. Run this script 
+    and it will allow for reloading and debugging
+    """
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, debug=True)
