b64a321667d3d175f06d4cb72d5bbbec99e37b14
Brandon Braner <brandon.braner@gmail.com>
2022-01-02 15:51:55 -0600

    moved main to root dir

--- {{cookiecutter.app_name}}/debug.py
+++ {{cookiecutter.app_name}}/debug.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     Used to debug via pycharm configuration. Run this script 
     and it will allow for reloading and debugging
     """
-    uvicorn.run('app.main:app', host='127.0.0.1', port=8000, debug=True)
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, debug=True)
